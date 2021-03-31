dfd364c96f562a1202e117e2bbf248b59140b39b
Jan-Philipp <janp@localhost.localdomain>
2021-03-31 22:39:15 +0200

    Fullscreen + set config

--- CameraWebServer/streamer.py
+++ CameraWebServer/streamer.py
@@ -1,5 +1,6 @@
 import sys
 import time
+import urllib.request
 from typing import List, Optional
 import numpy as np
 import cv2
@@ -7,11 +8,17 @@ import cv2
 base_url = "http://192.168.178.121"
 url = f"{base_url}:81/stream"
 
+ident = "framesize"
+value = 8   # XGA(1024x768)
+config_url = f"{base_url}/control?var={ident}&val={value}"
+resp = urllib.request.urlopen(config_url)
+
 cap = cv2.VideoCapture(url)
 
 window_name = "window"
-# cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
-# cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
+cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
 font = cv2.FONT_HERSHEY_SIMPLEX
 fontScale, color, thickness = 1, (255, 255, 255), 1
 
